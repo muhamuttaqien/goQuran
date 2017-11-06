c4188bbb822887a99249687a0a444d4fba7e1d0e
Muh. Angga Muttaqien <angga.muttaqien@ruma.co.id>
2017-11-06 17:12:05 +0700

    implement drawer and stack nativagor

---
+++ src/components/SideBar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  View
+} from 'react-native';
+
+import { Content, Text } from 'native-base';
+
+export default class SideBar extends Component {
+
+    render() {
+        return(
+            <Content style = {{ backgroundColor: '#ffffff' }}>
+
+            </Content>
+        )
+    };
+}
